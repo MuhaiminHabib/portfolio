bdb128beffe51ed2cebd3ac75f12e5b74be707cb
unknown <muhaimin.habib077@gmail.com>
2022-05-27 21:07:10 +0600

    done working on the nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,45 @@
 import './nav.css'
+import { useState } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { BsPerson, BsJournalBookmark, BsChatQuote } from 'react-icons/bs';
 import { RiHeartPulseLine } from 'react-icons/ri';
 
 const Nav = () => {
+
+  const [presentSection, setPresentSection] = useState('home');
+
   return (
     <nav>
-      <a href=""><AiOutlineHome /></a>
-      <a href=""><BsPerson /></a>
-      <a href=""><BsJournalBookmark /></a>
-      <a href=""><RiHeartPulseLine /></a>
-      <a href=""><BsChatQuote /></a>
+      <a 
+        href="#" 
+        className={presentSection === 'home' ? 'active' : ''}
+        onClick={() => setPresentSection('home')}>
+          <AiOutlineHome />
+      </a>
+      <a 
+        href="#about" 
+        className={presentSection === 'about' ? 'active' : ''}
+        onClick={() => setPresentSection('about')}>
+          <BsPerson />
+      </a>
+      <a 
+        href="#experience" 
+        className={presentSection === 'experience' ? 'active' : ''}
+        onClick={() => setPresentSection('experience')}>
+          <BsJournalBookmark />
+      </a>
+      <a
+        href="#services" 
+        className={presentSection === 'services' ? 'active' : ''}
+        onClick={() => setPresentSection('services')}>
+          <RiHeartPulseLine /> 
+      </a>
+      <a 
+        href="#contact" 
+        className={presentSection === 'contact' ? 'active' : ''}
+        onClick={() => setPresentSection('contact')}>
+          <BsChatQuote />
+      </a>
     </nav>
   )
 }
